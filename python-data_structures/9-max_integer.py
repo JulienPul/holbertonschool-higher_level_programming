171920db5324202c520c32953ab4bdc3adc38b4a
Pulon Julien <pulonjulien@gmail.com>
2025-05-12 15:37:04 +0200

    8-multiple_returns.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    Max_value = my_list[0]
+    for num in my_list:
+        if num > Max_value:
+            max_value = num
+            return num
