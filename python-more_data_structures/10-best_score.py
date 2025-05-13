0ea322daaaa7db62717bf7501fc6b8cd2c42307b
Pulon Julien <pulonjulien@gmail.com>
2025-05-13 14:45:44 +0200

    4-only_diff_elements.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    best_key = None
+    best_val = None
+    for key, value in a_dictionary.items():
+        if best_val is None or value > best_val:
+            best_key = key
+            best_val = value
+    return best_key
