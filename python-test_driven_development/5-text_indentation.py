d2ec569b3c76f670a0abc2aaaf2fbf3ae2436232
Pulon Julien <pulonjulien@gmail.com>
2025-05-16 15:42:19 +0200

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,12 @@ Defines a function text_indentation(text) that prints a string
 with two new lines after each of these characters: '.', '?', and ':'.
 """
 
+
 def text_indentation(text):
-    """Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
-Prototype: def text_indentation(text):
-"""
+    """Write a function that prints a text with 2 new lines after each of 
+    these characters: ., ? and :
+    Prototype: def text_indentation(text):
+    """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
