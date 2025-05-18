53902fc266239367c310ce5656ab80824d97d704
Pulon Julien <pulonjulien@gmail.com>
2025-05-18 12:49:43 +0200

    11-multiply_list_map.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,12 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-              'C': 100, 'D': 500, 'M': 1000}
+    values = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
+              'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
     numbers = list(map(values.get, roman_string))
-    result = 0
-    result = result + numbers
-    result = roman_to_int
-    return roman_to_int
+    for i in values:
+        result = 0
+        result = sum(numbers)
+        new_list = []
+        new_list = roman_string.append(result)
+    return new_list
