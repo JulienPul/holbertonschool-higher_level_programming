7daffeb93da50791185f4274e5dd4e58b488ddb7
Pulon Julien <pulonjulien@gmail.com>
2025-05-20 10:56:33 +0200

    3-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module 5-square:
+Define a class square with area method
+"""
+
+
+class Square:
+    """
+    define a square area by lenght side
+    """
+
+    def __init__(self, size=0):
+        """Private instance attribute: size:
+        property def size(self): to retrieve it
+        property setter def size(self, value): to set it:
+        size must be an integer, otherwise raise a TypeError exception
+        with the message size must be an integer
+        if size is less than 0, raise a ValueError exception
+        with the message size must be >= 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ property def size(self): to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """define size of the side with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
