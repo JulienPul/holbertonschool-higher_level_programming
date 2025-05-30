21a06b5ba2a3143f0155ae596b1236ca9ed76667
Pulon Julien <pulonjulien@gmail.com>
2025-05-30 12:32:03 +0200

    task_05_dragon.py

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -6,9 +6,11 @@ class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
+
 class FlyMixin:
     def fly(self):
         print("The creature flies!")
+
 
 class Dragon(SwimMixin, FlyMixin):
 
