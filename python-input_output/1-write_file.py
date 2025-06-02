e3b7f9fb20a5970be3dacbb819b0ed4a4965f4fc
Pulon Julien <pulonjulien@gmail.com>
2025-06-02 14:46:57 +0200

    4-from_json_string.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ returns the number of characters written."""
 def write_file(filename="", text=""):
     """function that writes a string to a text file
     (UTF8) and returns the number of characters written:"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         return f.write(text)
