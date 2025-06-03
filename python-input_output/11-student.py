0cf69e6830811d38a589bff1880f6743dbc5483b
Pulon Julien <pulonjulien@gmail.com>
2025-06-03 14:24:50 +0200

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """class Student that defines a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if key in self.__dict__:
+                self.__dict__[key] = json[key]
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     items = load_from_json_file(filename)
-except:
+except Exception:
     items = []
 
 items.extend(sys.argv[1:])
