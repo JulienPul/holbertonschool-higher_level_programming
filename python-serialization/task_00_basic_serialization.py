ecd70ffa59058439a7808d952df82eabfd6d2b5f
Pulon Julien <pulonjulien@gmail.com>
2025-06-03 15:00:22 +0200

    0-main.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,9 +4,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file."""
     with open(filename, "w") as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
+    """Load data from a JSON file and
+    deserialize it into a Python dictionary."""
     with open(filename, "r") as f:
         json.load(f)
