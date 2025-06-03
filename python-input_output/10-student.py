419f772b3fd2b37f608fffc8ea349595e40595dd
Pulon Julien <pulonjulien@gmail.com>
2025-06-03 12:07:11 +0200

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """class Student that defines a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        else:
+            return self.__dict__
