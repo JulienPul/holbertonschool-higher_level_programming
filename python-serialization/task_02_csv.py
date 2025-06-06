64c78215fad665aaa2b56a1190de780278517217
Pulon Julien <pulonjulien@gmail.com>
2025-06-06 14:10:02 +0200

    task_02_csv.py

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""mudule 2:Converting CSV Data to JSON Format"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """takes the CSV filename as its parameter """
+    try:
+        with open(filename, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file)
+        return True
+        
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return False
