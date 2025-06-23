18b0d5cc81f1cb511aae71baed6391e8aaeea9c8
Pulon Julien <pulonjulien@gmail.com>
2025-06-23 14:38:18 +0200

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- Displays score and name only, ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
