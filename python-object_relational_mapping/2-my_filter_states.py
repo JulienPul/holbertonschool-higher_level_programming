71cb5dad2d9062a0fcce67ccfb52ce022196dff8
Pulon Julien <pulonjulien@gmail.com>
2025-06-26 17:12:57 +0200

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-Lists all states where name matches the argument from the database hbtn_0e_0_usa.
-Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name>
+Displays all values in the states table where name matches the argument
+from the database hbtn_0e_0_usa.
 """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Arguments en ligne de commande
+    # Get command-line args
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connexion à la base de données
+    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,17 +23,16 @@ if __name__ == "__main__":
         db=db_name
     )
 
-    # Création du curseur
     cur = db.cursor()
-
-    # Requête SQL avec format (non sécurisé - volontaire)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    # Use .format() to build the query (non-safe version)
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(state_name)
+    )
     cur.execute(query)
 
-    # Affichage des résultats
     for row in cur.fetchall():
         print(row)
 
-    # Fermeture
     cur.close()
     db.close()
