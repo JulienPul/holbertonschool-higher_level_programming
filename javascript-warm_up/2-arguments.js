f6fe5e91408bd6d81c4821771d79d35dd8c22f4f
Pulon Julien <pulonjulien@gmail.com>
2025-07-11 21:54:49 +0200

    Add 2-arguments.js change lenght for length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 
-const args_Count = process.argv.lenght - 2;
+const args_Count = process.argv.length - 2;
 
-if (args_Count === 0) {
+if (args_Count === 0)
+    {
   console.log('No argument');
-} else if (args_Count === 1) {
+}
+ else if (args_Count === 1)
+    {
   console.log('Argument found');
-} else {
+}
+ else
+    {
   console.log('Arguments found');
 }
